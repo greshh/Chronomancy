68bf1ec7c6ed458649bf4a1248433220830ffc53
greshka lao <132567944+greshh@users.noreply.github.com>
2023-11-14 21:59:06 +1300

    remove acceleration

--- emnity.java
+++ emnity.java
@@ -9,15 +9,9 @@ public class emnity extends GameEngine {
 
     /* --- INTERACT FUNCTIONS --- */
     boolean jump, left, right;
-    boolean accelerate;
-
-    long leftTime, rightTime;
 
     @Override
     public void init() {
-        leftTime = 0;
-        rightTime = 0;
-
         jump = false;
         left = false;
         right = false;
@@ -30,45 +24,23 @@ public class emnity extends GameEngine {
 
     @Override
     public void paintComponent() {
-        // DEBUG
-        if (left && accelerate) { changeBackgroundColor(red);}
-        else if (left && !accelerate) { changeBackgroundColor(orange); }
-        else if (right && !accelerate) { changeBackgroundColor(blue); }
-        else if (right && accelerate) { changeBackgroundColor(purple); }
-        else { changeBackgroundColor(black); }
+        changeBackgroundColor(black);
         clearBackground(width(), height());
     }
     
     @Override
     public void keyPressed(KeyEvent e) 
     {
-        // updating left key bool - if LEFT is double-pressed (within 500ms), player is accelerated.
-        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-            if (getTime()-leftTime < 200) {
-                accelerate = true;
-            } else {
-                accelerate = false;
-            }
-            left = true;
-            leftTime = getTime();
-        }  
-
-        // updating right key bool - if RIGHT is double-pressed (within 500ms), player is accelerated.
-        if (e.getKeyCode() == KeyEvent.VK_RIGHT) { 
-            if (getTime()-rightTime < 200) {
-                accelerate = true;
-            } else {
-                accelerate = false;
-            }
-            right = true;
-            rightTime = getTime();
-        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) { left = true; }  
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) { right = true; }
+        // if (e.getKeyCode() == KeyEvent.VK_SPACE) { jump = true; }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_LEFT) { left = false; }  
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) { right = false; }
+        // if (e.getKeyCode() == KeyEvent.VK_SPACE) { jump = false; }
     }
 
     @Override
