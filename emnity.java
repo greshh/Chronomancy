c9e02bcd500e4a1dbd1411e0dde53b3f17851abb
greshka lao <132567944+greshh@users.noreply.github.com>
2023-11-15 15:51:03 +1300

    including vertical movement with final values

--- emnity.java
+++ emnity.java
@@ -7,9 +7,15 @@ public class emnity extends GameEngine {
         createGame(new emnity(), 60);
     }
 
-    boolean jump, left, right;
+    boolean jump, left, right, down;
+
+    final double GROUND = 450.0;
+    
     final double GRAVITY = 9.81;
-    final double ACCELERATION = 10.0;
+    final double HORIZONTAL_ACCELERATION = 10.0;
+    final double VERTICAL_ACCELERATION = 10.0;
+    final double MAX_VERTICAL_VELOCITY = 300;
+    final double MAX_HORIZONTAL_VELOCITY = 200;
 
     /* --- PLAYER --- */
     double playerX, playerY; // position of player.
@@ -17,7 +23,7 @@ public class emnity extends GameEngine {
 
     public void initPlayer() {
         playerX = width()/2;
-        playerY = 450;
+        playerY = GROUND;
         playerVX = 0;
         playerVY = 0;
     }
@@ -26,15 +32,18 @@ public class emnity extends GameEngine {
         playerX += playerVX*dt;
         playerY += playerVY*dt;
 
-        if (left) { 
-            if (playerVX > -200) { playerVX-= ACCELERATION; } 
-        } 
-        if (right) { 
-            if (playerVX < 200) { playerVX+= ACCELERATION; }
-        }
+        if (left) { if (playerVX > -MAX_HORIZONTAL_VELOCITY) { playerVX-= ACCELERATION; } } 
+        if (right) { if (playerVX < MAX_HORIZONTAL_VELOCITY) { playerVX+= ACCELERATION; } }
         if (!left && !right) { 
             if (playerVX < 0 && playerVX != 0) { playerVX+= ACCELERATION; } 
             if (playerVX > 0 && playerVX != 0) { playerVX-= ACCELERATION; }
+        }
+
+        if (jump) {
+            if (playerY > GROUND) {
+                playerY = GROUND;
+                jump = false;
+            } else { playerVY+= GRAVITY; }
         }
     }
 
@@ -48,6 +57,7 @@ public class emnity extends GameEngine {
         jump = false;
         left = false;
         right = false;
+        down = false;
         initPlayer();
 
         System.out.println(width() + " " + height());
@@ -70,14 +80,18 @@ public class emnity extends GameEngine {
     {
         if (e.getKeyCode() == KeyEvent.VK_LEFT) { left = true; }  
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) { right = true; }
-        // if (e.getKeyCode() == KeyEvent.VK_SPACE) { jump = true; }
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) { 
+            if (!jump) {
+                jump = true; 
+                playerYV = MAX_VERTICAL_VELOCITY;
+            }
+        }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_LEFT) { left = false; }  
         if (e.getKeyCode() == KeyEvent.VK_RIGHT) { right = false; }
-        // if (e.getKeyCode() == KeyEvent.VK_SPACE) { jump = false; }
     }
 
     @Override
